79f11c4738df2d38976c41ad0704ea6a792ab70e
Elias Kounakas <eliaskounakas@proton.me>
2024-02-15 13:11:04 +0100

    setup sql URL with .env

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
+
+SQL_URL = config('SQL_URL')
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = ""
+SQLALCHEMY_DATABASE_URL = SQL_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
