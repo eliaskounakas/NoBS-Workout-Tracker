4eb847b1ddfafe940dba8235433963e797bad8b2
elias <eliaskounakas@proton.me>
2024-02-21 15:33:40 +0100

    Added user creation with encrypted passwords

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel
 from typing import List, Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+import auth
 
 app = FastAPI()
+app.include_router(auth.router)
+
+models.Base.metadata.create_all(bind=engine)
 
 class Users(BaseModel):
     username: str
@@ -11,4 +18,18 @@ class Users(BaseModel):
 class Exercises(BaseModel):
     name: str
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
     
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(user: None, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    return {"User": user}
+
--- sql_app/models.py
+++ sql_app/models.py
@@ -1,4 +1,17 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from database import Base 
 
-class users
+class Users(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    password = Column(String)
+
+class Exercises(Base):
+    __tablename__ = 'exercises'
+
+    exercise_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    name = Column(String) 
+    
