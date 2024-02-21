8d95a0ccc7c3127451b2afedcb5890476f685b66
elias <eliaskounakas@proton.me>
2024-02-21 15:59:09 +0100

    Add JWT user verification

--- sql_app/auth.py
+++ sql_app/auth.py
@@ -18,7 +18,7 @@ router = APIRouter(
 )
 
 SECRET_KEY = config('SECRET_KEY')
-ALGORITHM = 'HS256'
+ALGORITHM = config('ALGORITHM')
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
@@ -28,7 +28,7 @@ class CreateUserRequest(BaseModel):
     password: str
 
 class Token(BaseModel):
-    acces_token: str
+    access_token: str
     token_type: str
 
 def get_db():
@@ -50,3 +50,44 @@ async def create_user(db: db_dependency,
 
     db.add(create_user_model)
     db.commit()
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
+    token = create_access_token(user.username, user.user_id, timedelta(minutes=30))
+
+    return {'access_token': token, 'token_type': 'bearer'}
+
+
+
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.password):
+        return False
+    return user
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.utcnow() + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.')
+        return {'username': username, 'id': user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.')
--- sql_app/main.py
+++ sql_app/main.py
@@ -5,6 +5,7 @@ import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 import auth
+from auth import get_current_user
 
 app = FastAPI()
 app.include_router(auth.router)
@@ -26,9 +27,10 @@ def get_db():
         db.close
     
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @app.get("/", status_code=status.HTTP_200_OK)
-async def user(user: None, db: db_dependency):
+async def user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
     return {"User": user}
